6b91e5f08c77a1f6484c115264f1e0791fb5ba5a
LucasIbargoyen <62619004+LucasIbargoyen@users.noreply.github.com>
2025-05-27 21:33:37 -0300

    Add files via upload

---
+++ buscaBinaria.pas
@@ -0,0 +1,23 @@
+Program Pzim ;
+const
+	tamanho := 10;
+	
+var
+	elementos: array[1...tamanho] of Integer;
+	opcao: Integer;
+	
+function calcularEndereco(inicio, fim: Integer): Integer;
+begin
+	calcularEndereco := (inicio + fim) div 2;
+	
+	
+	
+
+
+
+
+
+	
+Begin
+  
+End.
